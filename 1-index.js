97df8787ecf03ff14a6d6b0c331d2a8650313079
Sofia Protsiv <prochivsofia@gmail.com>
2023-07-18 13:49:44 +0300

    feat: add materials for 01-01

---
+++ 1-index.js
@@ -0,0 +1,16 @@
+/*
+ * Базові примітивні типи: number, string, boolean, null и undefined
+ * typeof
+*/
+
+/*
+ * Виведення даних: console.log та alert. Вкладка Console у Chrome Devtools.
+*/
+
+/*
+ * Змінні: оголошення з let та const, іменування.
+*/
+
+/*
+ * Отримання даних: prompt и confirm.
+*/
---
+++ 2-index.js
@@ -0,0 +1,59 @@
+"use strict";
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Strict_mode
+
+/*
+ * Математичні оператори (+, -, *, /, %, **)
+ */
+
+/*
+ ? Дано два числа 10 и 20.
+ ? Проведіть над цими числами математичні операції (+ - / *).
+*/
+const a = 20;
+const b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+/*
+ ? Перевірте чи парні числа 15, 20, 45, 30.
+*/
+
+/*
+  ? Дано 90 хвилин, дізнайтесь скільки в даному числі міститься годин і хвилин.
+  ? Результат виведіть у консоль.
+*/
+
+/*
+ * Number.parseInt() Number.parseFloat()
+ */
+
+/*
+  ? Попросіть користувача ввести число.
+  ? Виведіть результат у консоль.
+  ? Приведіть рядок до числа.
+  ? Дістаньте з цього рядка ціле число / дробове число.
+*/
+
+/*
+  ? Спробуйте привести до числа рядок 'abc'. (typeof)
+*/
+
+/*
+ * Math.floor, Math.ceil, Math.round, Math.max, Math.min, Math.pow(), Math.random().
+ */
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.9)); 
+// console.log(Math.round(3.5)); 
+// console.log(Math.round(3.2)); 
+// console.log((12345.6859).toFixed(2));
+// console.log(Math.max(20, 10, 50, 40, 94));
+// console.log(Math.min(20, 10, 50, 40, 94, -4)); 
+// console.log(Math.pow(2, 4));
+// console.log(2 ** 4);
+
+// const max = 10;
+// const min = 1;
+// console.log(Math.floor(Math.random() * (max - min) + min));
+// console.log(Math.floor(Math.random() * 25));
